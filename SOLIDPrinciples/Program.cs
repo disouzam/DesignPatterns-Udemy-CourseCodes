fceef16800acf996d555913a7b7f65416292776f
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-27 14:57:59 -0300

    Violation of the principle

--- SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/Program.cs
@@ -6,6 +6,54 @@ using static System.Console;
 
 namespace SOLIDPrinciples
 {
+    public class Document
+    {
+
+    }
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+
+    }
+
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
