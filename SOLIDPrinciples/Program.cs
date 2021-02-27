8695afba0a3f91ac116e7f44a59d6c1993f7e1df
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-27 15:38:42 -0300

    Violation of Dependency Inversion Principle

--- SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/Program.cs
@@ -2,15 +2,64 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using static System.Console;
 
 namespace SOLIDPrinciples
 {
-    class Program
+    public enum Relationship
     {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public class Person
+    {
+        public string Name;
+        // public DateTime DateOfBirth;
+    }
+
+    // low-level
+
+    public class Relationships
+    {
+        private List<(Person, Relationship, Person)> relations 
+            = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
+        public List<(Person, Relationship, Person)> Relations => relations;
+    }
+
+    class Research
+    {
+        public Research(Relationships relationships)
+        {
+            var relations = relationships.Relations;
+            foreach(var r in relations.Where(
+                x => x.Item1.Name == "John" &&
+                x.Item2 == Relationship.Parent))
+            {
+                WriteLine($"John as a child called {r.Item3.Name}");
+            }
+        }
         static void Main(string[] args)
         {
-                     
+            var parent = new Person { Name = "John" };
+            var child1 = new Person { Name = "Chris" };
+            var child2 = new Person { Name = "Mary" };
+
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
+
+            new Research(relationships);
         }
     }
 }
+
