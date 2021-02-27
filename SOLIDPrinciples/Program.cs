0c8cb06f4dacdb619ff948203ad8cb0f0eca8e14
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-27 13:22:00 -0300

    Removing previous code - Fron Single Responsibility Principle

--- SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/Program.cs
@@ -5,55 +5,12 @@ using System.IO;
 using static System.Console;
 
 namespace SOLIDPrinciples
-{
-    public class Journal
-    {
-        private readonly List<string> entries = new List<string>();
-
-        private static int count = 0;
-
-        public int AddEntry(string text)
-        {
-            entries.Add($"{++count}: {text}");
-            return count; // memento
-        }
-
-        public void RemoveEntry(int index)
-        {
-            entries.RemoveAt(index);
-        }
-
-        public override string ToString()
-        {
-            return string.Join(Environment.NewLine, entries);
-        }
-
-
-
-    }
-
-    public class Persistence
-    {
-        public void SaveToFile (Journal j, string filename, bool overwrite = false)
-        {
-            if (overwrite || !File.Exists(filename))
-                File.WriteAllText(filename, j.ToString());
-        }
-    }
-
+{   
     class Program
     {
         static void Main(string[] args)
         {
-            var j = new Journal();
-            j.AddEntry("I cried today");
-            j.AddEntry("I ate a bug");
-            WriteLine(j);
-
-            var p = new Persistence();
-            var filename = @"C:\LearningProjects_Repos\DesignPatterns\CourseCodes\Resources\journal.txt";
-            p.SaveToFile(j, filename, true);
-            Process.Start("notepad", filename);
+            
         }
     }
 }
