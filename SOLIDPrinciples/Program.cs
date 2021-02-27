0b5a70c9d60758db431986c52eaaaa2b8fdedda9
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-27 14:47:29 -0300

    Removing previous code - From Liskov Substitution Principle

--- SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/Program.cs
@@ -6,50 +6,11 @@ using static System.Console;
 
 namespace SOLIDPrinciples
 {
-    public class Rectangle
-    {
-        public virtual int Width { get; set; }
-        public virtual int Height { get; set; }
-
-        public Rectangle()
-        {
-
-        }
-
-        public Rectangle(int width, int height)
-        {
-            Width = width;
-            Height = height;
-        }
-        public override string ToString()
-        {
-            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
-        }
-    }
-
-    public class Square : Rectangle
-    {
-        public override int Width
-        {
-            set { base.Width = base.Height = value; }
-        }
-        public override int Height
-        {
-            set { base.Width = base.Height = value; }
-        }
-    }
     class Program
     {
-        static public int Area(Rectangle r) => r.Width * r.Height;
-
         static void Main(string[] args)
         {
-            Rectangle rc = new Rectangle(2,3);
-            WriteLine($"{rc} has area {Area(rc)}");
-
-            Rectangle sq = new Square();
-            sq.Width = 4;
-            WriteLine($"{sq} has area {Area(sq)}");
+                     
         }
     }
 }
