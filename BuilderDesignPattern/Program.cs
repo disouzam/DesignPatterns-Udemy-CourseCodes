7c210fd641cdc9479699dd56713d65cc6e19fdb2
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-27 19:22:36 -0300

    Created the first project to use in Builder set of design patterns

---
+++ BuilderDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuilderDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
