9d6cef5d2ae6c2d1d8ee652f06b6bb6e23948ef9
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-27 10:57:11 -0300

    Coding Journal class but violating Single Responsibility Principle

--- SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Console;
 
 namespace SOLIDPrinciples
 {
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public static Journal Load(string filename)
+        {
+
+        }
+
+        public void Load (Uri uri)
+        {
+
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var j = new Journal();
+            j.AddEntry("I cried today");
+            j.AddEntry("I ate a bug");
+            WriteLine(j);
         }
     }
 }
