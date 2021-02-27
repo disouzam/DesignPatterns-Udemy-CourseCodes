3cecc7a8f34aa1c5894f6495072601f1f7588bb2
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-27 14:04:59 -0300

    Final implementation of Open Closed Principle

--- SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/Program.cs
@@ -53,6 +53,81 @@ namespace SOLIDPrinciples
                         yield return p;
                 }
             }
+
+            public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+            {
+                foreach (var p in products)
+                {
+                    if (p.Size == size && p.Color == color)
+                        yield return p;
+                }
+            }
+        }
+
+        public interface ISpecification<T>
+        {
+            bool IsSatisfied(T t);
+        }
+
+        public interface IFilter<T>
+        {
+            IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+        }
+
+        public class ColorSpecification: ISpecification<Product>
+        {
+            private Color color;
+             
+            public ColorSpecification(Color color)
+            {
+                this.color = color;
+            }
+
+            public bool IsSatisfied (Product t)
+            {
+                return t.Color == color;
+            }
+        }
+
+        public class SizeSpecification : ISpecification<Product>
+        {
+            private Size size;
+
+            public SizeSpecification(Size size)
+            {
+                this.size = size;
+            }
+
+            public bool IsSatisfied(Product t)
+            {
+                return t.Size == size;
+            }
+        }
+
+        public class AndSpecification<T> : ISpecification<T>
+        {
+            private ISpecification<T> first, second;
+
+            public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+            {
+                this.first = first ?? throw new ArgumentNullException(nameof(first));
+                this.second = second ?? throw new ArgumentNullException(nameof(second));
+            }
+
+            public bool IsSatisfied(T t)
+            {
+                return first.IsSatisfied(t) && second.IsSatisfied(t);
+            }
+        }
+
+        public class BetterFilter : IFilter<Product>
+        {
+            public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+            {
+                foreach (var i in items)
+                    if (spec.IsSatisfied(i))
+                        yield return i;
+            }
         }
 
         static void Main(string[] args)
@@ -68,6 +143,23 @@ namespace SOLIDPrinciples
             foreach (var p in pf.FilterByColor(products, Color.Green))
             {
                 WriteLine($" - {p.Name} is green");
+            }
+
+            var bf = new BetterFilter();
+            WriteLine("Green products (new):");
+            foreach(var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                WriteLine($" - {p.Name} is green");
+            }
+
+            WriteLine("Large blue items");
+            foreach (var p in bf.Filter(
+                products,
+                new AndSpecification<Product>(
+                    new ColorSpecification(Color.Blue),
+                    new SizeSpecification(Size.Large))))
+            {
+                WriteLine($" - {p.Name} is big and blue");
             }
         }
     }
