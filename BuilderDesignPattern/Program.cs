da2f84b2204e1470d2424f20e39c7e6d781aa692
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-27 23:19:21 -0300

    Without a builder

--- BuilderDesignPattern/Program.cs
+++ BuilderDesignPattern/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using static System.Console;
+using System.Text;
 
 namespace BuilderDesignPattern
 {
-    class Program
+    public class Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var hello = "hello";
+            var sb = new StringBuilder();
+
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            WriteLine(sb);
+
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach(var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);            
+            }
+            sb.Append("</ul>");
+            WriteLine(sb);
         }
     }
 }
