5f27ef339c6baf418da7bc32c6dc7ab053b8f87e
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-28 14:04:10 -0300

    Clean file to start lecture

--- BuilderDesignPattern/Program.cs
+++ BuilderDesignPattern/Program.cs
@@ -6,58 +6,11 @@ using static System.Console;
 
 namespace BuilderDesignPattern
 {
-    public class Person
-    {
-        public string Name, Position;
-    }
-
-    public abstract class FunctionalBuilder<TSubject, TSelf>
-        where TSelf: FunctionalBuilder<TSubject, TSelf>
-        where TSubject: new()
-    {
-        private readonly List<Func<Person, Person>> actions
-            = new List<Func<Person, Person>>();
-
-        public TSelf Do(Action<Person> action)
-            => AddAction(action);
-
-        public Person Build() => actions.Aggregate(new Person(), (p, f) => f(p));
-
-        private TSelf AddAction(Action<Person> action)
-        {
-            actions.Add(p =>
-            {
-                action(p);
-                return p;
-            });
-            return (TSelf) this;
-        }
-    }
-
-    public sealed class PersonBuilder
-        : FunctionalBuilder<Person, PersonBuilder>
-    {
-        public PersonBuilder Called(string name)
-        => Do(p => p.Name = name);
-    }
-
-    public static class PersonBuilderExtensions
-    {
-        public static PersonBuilder WorksAs
-            (this PersonBuilder builder, string position)
-            => builder.Do(p => p.Position = position);
-    }
-
     public class Demo
     {
         static void Main(string[] args)
         {
-            var person = new PersonBuilder()
-                .Called("Sarah")
-                .WorksAs("Developer")
-                .Build();
-
-            WriteLine(person);
+           
         }
     }
 }
