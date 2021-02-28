3538847306158271179f0e57187fdd7d522946ba
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-28 11:46:17 -0300

    Initial implementation of Functional Builder

--- BuilderDesignPattern/Program.cs
+++ BuilderDesignPattern/Program.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using static System.Console;
 
 namespace BuilderDesignPattern
 {
-
+    public class Person
+    {
+        public string Name, Position;
+    }
+    public sealed class PersonBuilder
+    {
+        private readonly List<Func<Person, Person>> actions
+            = new List<Func<Person, Person>>();
+    }
     public class Demo
     {
         static void Main(string[] args)
