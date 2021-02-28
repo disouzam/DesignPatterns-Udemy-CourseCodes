7fce776e4c8f6e25e86cec682c8a08b4eb25e868
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-02-28 10:47:45 -0300

    Wrong implementation because the base class fluent builder returns a reference to base class and not to derived class and that avoids accessing WorkAsA after Called method

--- BuilderDesignPattern/Program.cs
+++ BuilderDesignPattern/Program.cs
@@ -6,9 +6,39 @@ namespace BuilderDesignPattern
 {
     public class Demo
     {
+        public class Person
+        {
+            public string Name;
+            public string Position;
+            public override string ToString()
+            {
+                return $"{nameof(Name)}: {Name},{nameof(Position)}: {Position}";
+            }
+        }
+        public class PersonInfoBuilder
+        {
+            protected Person person = new Person();
+
+            public PersonInfoBuilder Called(string name)
+            {
+                person.Name = name;
+                return this;
+            }
+        } 
+
+        public class PersonJobBuilder: PersonInfoBuilder
+        {
+            public PersonJobBuilder WorkAsA(string position)
+            {
+                person.Position = position;
+                return this;
+            }
+        }
         static void Main(string[] args)
         {
-
+            var builder = new PersonJobBuilder();
+            builder.Called("Dickson")
+                .WorkAsA
         }
     }
 }
