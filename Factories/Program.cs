d48e499351983160253f373a5d219f6f9d79d18b
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-04 21:19:40 -0300

    Adding a class point to describe a geometrical point

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -4,6 +4,16 @@ namespace Factories
 {
     class Program
     {
+        public class Point
+        {
+            private double x, y;
+
+            public Point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
