2e8414031ad6ac6f8a1b7b7d9bf303a3a88927fa
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-04 22:35:48 -0300

    Full implementation of Async factory

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -5,23 +5,31 @@ namespace Factories
 {
     public class Foo
     {
-        public Foo()
+        private Foo()
         {
-            
+            //
         }
 
-        public async Task<Foo> InitAsync()
+        private async Task<Foo> InitAsync()
         {
             await Task.Delay(1000);
             return this;
+        }
+
+        // factory method
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
         }
     }
     class Program
     {
         static async Task Main(string[] args)
         {
-            var foo = new Foo();
-            await foo.InitAsync();
+            //var foo = new Foo();
+            //await foo.InitAsync();
+            Foo x = await Foo.CreateAsync();
         }
     }
 }
