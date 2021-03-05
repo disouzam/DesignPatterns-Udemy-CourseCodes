ea3c66b145631bc7169e54237df2aac9d86de55d
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-04 21:32:20 -0300

    Adding a factory method to create a cartesian point

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -7,11 +7,16 @@ namespace Factories
         public class Point
         {
             private double x, y;
-
-            public Point(double x, double y)
+            
+            private Point(double x, double y)
             {
                 this.x = x;
                 this.y = y;
+            }
+
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
             }
         }
         static void Main(string[] args)
