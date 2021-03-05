6acb909ac1068c832a79f7d84be2569d93a66398
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-04 21:35:24 -0300

    Added another factory method to create a point passing polar coordinates

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -14,9 +14,16 @@ namespace Factories
                 this.y = y;
             }
 
+            // factory method
             public static Point NewCartesianPoint(double x, double y)
             {
                 return new Point(x, y);
+            }
+
+            // factory method
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho*Math.Sin(theta));
             }
         }
         static void Main(string[] args)
