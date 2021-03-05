33823ad3941b11cba269479c773dacd6c5a42425
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-04 22:38:30 -0300

    Cleaning up project to start another lecture

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -3,33 +3,12 @@ using static System.Console;
 
 namespace Factories
 {
-    public class Foo
-    {
-        private Foo()
-        {
-            //
-        }
-
-        private async Task<Foo> InitAsync()
-        {
-            await Task.Delay(1000);
-            return this;
-        }
-
-        // factory method
-        public static Task<Foo> CreateAsync()
-        {
-            var result = new Foo();
-            return result.InitAsync();
-        }
-    }
+    
     class Program
     {
-        static async Task Main(string[] args)
+        public void Main(string[] args)
         {
-            //var foo = new Foo();
-            //await foo.InitAsync();
-            Foo x = await Foo.CreateAsync();
+
         }
     }
 }
