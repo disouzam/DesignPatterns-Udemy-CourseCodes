90c4f6e05ebefbf3c8fb3d1c7f09fcf1970bd067
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-04 21:45:09 -0300

    Transforming Point and Angle classes to be under namespace Factories and not under class Program

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -2,33 +2,43 @@
 
 namespace Factories
 {
+    public static class Angle
+    {
+        public static double ToRadians(double degree)
+        {
+            return (Math.PI * degree / 180.0);
+        }
+    }
+
+    public class Point
+    {
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // factory method
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        // factory method
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+    }
+
     class Program
     {
-        public class Point
-        {
-            private double x, y;
-            
-            private Point(double x, double y)
-            {
-                this.x = x;
-                this.y = y;
-            }
-
-            // factory method
-            public static Point NewCartesianPoint(double x, double y)
-            {
-                return new Point(x, y);
-            }
-
-            // factory method
-            public static Point NewPolarPoint(double rho, double theta)
-            {
-                return new Point(rho * Math.Cos(theta), rho*Math.Sin(theta));
-            }
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var point = Point.NewPolarPoint(1, Angle.ToRadians(Math.PI / 2));
+            WriteLine(point);
         }
     }
 }
