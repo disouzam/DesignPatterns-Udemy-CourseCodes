94aee1a2599fa24eaee0f6b3498181e95b697f22
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-04 22:30:49 -0300

    Removing previous code and adding async calls in a class - not the final solution

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -1,49 +1,27 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using static System.Console;
 
 namespace Factories
 {
-    public static class Angle
+    public class Foo
     {
-        public static double ToRadians(double degree)
+        public Foo()
         {
-            return (Math.PI * degree / 180.0);
+            
+        }
+
+        public async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
         }
     }
-
-    public class Point
-    {
-        private double x, y;
-
-        private Point(double x, double y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        // factory method
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
-
-        // factory method
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
-
-        public override string ToString()
-        {
-            return $"{nameof(x)}:{x}, {nameof(y)}: {y}";
-        }
-    }
-
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var point = Point.NewPolarPoint(1, Angle.ToRadians(Math.PI / 2));
-            Console.WriteLine(point);
+            var foo = new Foo();
+            await foo.InitAsync();
         }
     }
 }
