625187f952d8140a65eac5381ba5191d892b753c
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-06 14:51:10 -0300

    Concrete class factory skeleton

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -3,7 +3,18 @@ using static System.Console;
 
 namespace Factories
 {
-    
+    class Point
+    {
+
+    }
+
+    class PointFactory
+    {
+        Point CreateWhatever()
+        {
+
+        }
+    }
     class Program
     {
         public void Main(string[] args)
