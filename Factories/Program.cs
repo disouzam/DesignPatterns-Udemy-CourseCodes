8f93b36134728e912d353c9a2226210b912d9445
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-06 16:08:27 -0300

    Removing comments to clean the code"

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -49,29 +49,6 @@ namespace Factories
 
     public class HotDrinkMachine
     {
-        //public enum AvailableDrink
-        //{
-        //    Coffee, Tea
-        //}
-
-        //private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
-        //    new Dictionary<AvailableDrink, IHotDrinkFactory>();
-
-        //public HotDrinkMachine()
-        //{
-        //    foreach(AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
-        //    {
-        //        var factory = (IHotDrinkFactory)Activator.CreateInstance(
-        //            Type.GetType("Factories." + Enum.GetName(typeof(AvailableDrink), drink)+ "Factory"));
-        //        factories.Add(drink, factory);
-        //    }
-        //}
-
-        //public IHotDrink MakeDrink(AvailableDrink drink, int amount)
-        //{
-        //    return factories[drink].Prepare(amount);
-        //}
-
         private List<Tuple<string, IHotDrinkFactory>> factories =
             new List<Tuple<string, IHotDrinkFactory>>();
 
