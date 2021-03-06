29b63af385db0ab76bb618501a2880a63bb57ad6
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-06 19:38:28 -0300

    Exercise implementation

--- Prototypes/Program.cs
+++ Prototypes/Program.cs
@@ -1,5 +1,28 @@
-﻿namespace Prototypes
+﻿namespace Prototypes.Coding.Exercise
 {
+    public class Point
+    {
+        public int X, Y;
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            Line nl = new Line();
+            nl.Start = new Point();
+            nl.Start.X = Start.X;
+            nl.Start.Y = Start.Y;
+
+            nl.End = new Point();
+            nl.End.X = End.X;
+            nl.End.Y = End.Y;
+            return nl;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
