ff5c682662322cae432f127b9b1da5a647715e3a
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-05 22:00:39 -0300

    Inefficient asynchronous implementation

--- AsyncProgram/Program.cs
+++ AsyncProgram/Program.cs
@@ -11,6 +11,12 @@ namespace AsyncProgram
             Console.WriteLine("Pouring coffee");
             return new Coffee();
         }
+
+        public static Coffee PourCoffeeAsync()
+        {
+            Console.WriteLine("Pouring coffee Asynchronously");
+            return new Coffee();
+        }
     }
 
     public class Egg
@@ -22,6 +28,17 @@ namespace AsyncProgram
             Console.WriteLine($"cracking {howMany} eggs");
             Console.WriteLine("cooking the eggs");
             Task.Delay(3000).Wait();
+
+            return new Egg();
+        }
+
+        public static async Task<Egg> FryEggsAsync(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan Asynchronously...");
+            await Task.Delay(3000);
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs");
+            await Task.Delay(3000);
 
             return new Egg();
         }
@@ -40,6 +57,22 @@ namespace AsyncProgram
             }
             Console.WriteLine("cooking the second side of bacon...");
             Task.Delay(3000).Wait();
+            Console.WriteLine("Put bacon on plate");
+
+            return new Bacon();
+        }
+
+        public static async Task<Bacon> FryBaconAsync(int slices)
+        {
+            Console.WriteLine($"putting {slices} slices of bacon in the pan Asynchronously");
+            Console.WriteLine("cooking first side of bacon...");
+            await Task.Delay(3000);
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("flipping a slice of bacon");
+            }
+            Console.WriteLine("cooking the second side of bacon...");
+            await Task.Delay(3000);
             Console.WriteLine("Put bacon on plate");
 
             return new Bacon();
@@ -66,6 +99,19 @@ namespace AsyncProgram
 
             return new Toast();
         }
+
+        public static async Task<Toast> ToastBreadAsync(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster Asynchronously");
+            }
+            Console.WriteLine("Start toasting...");
+            await Task.Delay(3000);
+            Console.WriteLine("Remove toast from toaster");
+
+            return new Toast();
+        }
     }
 
     public class Juice
@@ -73,6 +119,12 @@ namespace AsyncProgram
         public static Juice PourOJ()
         {
             Console.WriteLine("Pouring orange juice");
+            return new Juice();
+        }
+
+        public static Juice PourOJAsync()
+        {
+            Console.WriteLine("Pouring orange juice Asynchronously");
             return new Juice();
         }
 
@@ -85,21 +137,24 @@ namespace AsyncProgram
             DateTime initTime = DateTime.Now;
             Console.WriteLine(initTime);
 
-            Coffee cup = Coffee.PourCoffee();
+            Coffee cup = Coffee.PourCoffeeAsync();
             Console.WriteLine("coffee is ready");
 
-            Egg eggs = Egg.FryEggs(2);
+            Task<Egg> eggsTask = Egg.FryEggsAsync(2);
+            Egg eggs = await eggsTask;
             Console.WriteLine("eggs are ready");
 
-            Bacon bacon = Bacon.FryBacon(3);
+            Task<Bacon> baconTask = Bacon.FryBaconAsync(3);
+            Bacon bacon = await baconTask;
             Console.WriteLine("bacon is ready");
 
-            Toast toast = Toast.ToastBread(2);
+            Task<Toast> toastTask = Toast.ToastBreadAsync(2);
+            Toast toast = await toastTask;
             Toast.ApplyButter(toast);
             Toast.ApplyJam(toast);
             Console.WriteLine("toast is ready");
 
-            Juice oj = Juice.PourOJ();
+            Juice oj = Juice.PourOJAsync();
             Console.WriteLine("Orange juice is ready");
             Console.WriteLine("Breakfast is ready!");
 
