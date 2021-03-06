ed742f7cfd28aeee1a4d4aa2481c517c86dc096b
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-06 15:39:19 -0300

    Commenting out the rest of class HotDrinkMachine

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -54,23 +54,23 @@ namespace Factories
         //    Coffee, Tea
         //}
 
-        private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
-            new Dictionary<AvailableDrink, IHotDrinkFactory>();
+        //private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
+        //    new Dictionary<AvailableDrink, IHotDrinkFactory>();
 
-        public HotDrinkMachine()
-        {
-            foreach(AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
-            {
-                var factory = (IHotDrinkFactory)Activator.CreateInstance(
-                    Type.GetType("Factories." + Enum.GetName(typeof(AvailableDrink), drink)+ "Factory"));
-                factories.Add(drink, factory);
-            }
-        }
+        //public HotDrinkMachine()
+        //{
+        //    foreach(AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        //    {
+        //        var factory = (IHotDrinkFactory)Activator.CreateInstance(
+        //            Type.GetType("Factories." + Enum.GetName(typeof(AvailableDrink), drink)+ "Factory"));
+        //        factories.Add(drink, factory);
+        //    }
+        //}
 
-        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
-        {
-            return factories[drink].Prepare(amount);
-        }
+        //public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        //{
+        //    return factories[drink].Prepare(amount);
+        //}
     }
 
     class Program
