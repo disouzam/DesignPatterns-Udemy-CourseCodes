a5ca33cab88783f423ad45969f4cdf43ec3c25b8
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-06 16:49:38 -0300

    Exercise solution

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -1,13 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 using static System.Console;
 
-namespace Factories
+namespace Coding.Exercise
 {
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}";
+        }
+    }
+
+    public class PersonFactory
+    {
+        private static int count = 0;
+
+        public Person CreatePerson(string name)
+        {
+            var p = new Person();
+            p.Id = count;
+            count++;
+            p.Name = name;
+            return p;
+        }                
+    }
+
     class Program
     {
         public static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("What is the name of the person to be created?");
+                string s = Console.ReadLine();
+                PersonFactory pf = new PersonFactory();
+                var p = pf.CreatePerson(s);
+                WriteLine(p);
+            }
         }
     }
 }
