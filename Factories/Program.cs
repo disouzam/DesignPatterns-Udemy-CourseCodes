63411d0f227232ae8aee2bc018423690cf027a7e
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-06 16:18:17 -0300

    Preparing a clean project to do the coding exercise

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -4,107 +4,10 @@ using static System.Console;
 
 namespace Factories
 {
-    public interface IHotDrink
-    {
-        void consume();
-    }
-    internal class Tea : IHotDrink
-    {
-        void IHotDrink.consume()
-        {
-            WriteLine("This tea is nice but I'd prefer it with milk.");
-        }
-    }
-
-    internal class Coffee : IHotDrink
-    {
-        public void consume()
-        {
-            WriteLine("This coffee is sensational!");
-        }
-    }
-
-    public interface IHotDrinkFactory
-    {
-        IHotDrink Prepare(int amount);
-    }
-
-    internal class TeaFactory : IHotDrinkFactory
-    {
-        IHotDrink IHotDrinkFactory.Prepare(int amount)
-        {
-            WriteLine($"Put in a tea bag, boil water, pour {amount} ml, add lemon, enjoy!");
-            return new Tea();
-        }
-    }
-
-    internal class CoffeeFactory : IHotDrinkFactory
-    {
-        IHotDrink IHotDrinkFactory.Prepare(int amount)
-        {
-            WriteLine($"Grind some beans, boil water, pour {amount} ml, add cream and sugar, enjoy!");
-            return new Coffee();
-        }
-    }
-
-    public class HotDrinkMachine
-    {
-        private List<Tuple<string, IHotDrinkFactory>> factories =
-            new List<Tuple<string, IHotDrinkFactory>>();
-
-        public HotDrinkMachine()
-        {
-            foreach(var t in typeof(HotDrinkMachine).Assembly.GetTypes())
-            {
-                if(typeof(IHotDrinkFactory).IsAssignableFrom(t) &&
-                    !t.IsInterface)
-                {
-                    factories.Add(Tuple.Create(
-                        t.Name.Replace("Factory", string.Empty),
-                        (IHotDrinkFactory)Activator.CreateInstance(t)
-                        ));
-                }
-            }
-        }
-
-        public IHotDrink MakeDrink()
-        {
-            WriteLine("Available drinks:");
-            for(var index = 0; index < factories.Count; index++)
-            {
-                var tuple = factories[index];
-                WriteLine($"{index}: {tuple.Item1}");
-            }
-
-            while (true)
-            {
-                string s;
-                if ((s = Console.ReadLine()) != null
-                    && int.TryParse(s, out int i)
-                    && i >= 0
-                    && i < factories.Count)
-                {
-                    Write("Specify amount: ");
-                    s = ReadLine();
-                    if (s != null
-                        && int.TryParse(s, out int amount)
-                        && amount > 0)
-                    {
-                        return factories[i].Item2.Prepare(amount);
-                    }
-                }
-                WriteLine("Incorrect input, try again!");
-            }
-        } 
-    }
-
     class Program
     {
         public static void Main(string[] args)
         {
-            var machine = new HotDrinkMachine();
-            var drink = machine.MakeDrink();
-            drink.consume();
         }
     }
 }
