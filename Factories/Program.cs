4455a8424903af2c4e5e8bc8bef5a09172c02ba7
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-06 15:21:05 -0300

    Implementation of an abstract factory

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -1,25 +1,85 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace Factories
 {
-    class Point
+    public interface IHotDrink
     {
-
+        void consume();
     }
-
-    class PointFactory
+    internal class Tea : IHotDrink
     {
-        Point CreateWhatever()
+        void IHotDrink.consume()
         {
-
+            WriteLine("This tea is nice but I'd prefer it with milk.");
         }
     }
+
+    internal class Coffee : IHotDrink
+    {
+        public void consume()
+        {
+            WriteLine("This coffee is sensational!");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        IHotDrink IHotDrinkFactory.Prepare(int amount)
+        {
+            WriteLine($"Put in a tea bag, boil water, pour {amount} ml, add lemon, enjoy!");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        IHotDrink IHotDrinkFactory.Prepare(int amount)
+        {
+            WriteLine($"Grind some beans, boil water, pour {amount} ml, add cream and sugar, enjoy!");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        public enum AvailableDrink
+        {
+            Coffee, Tea
+        }
+
+        private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
+            new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        public HotDrinkMachine()
+        {
+            foreach(AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+            {
+                var factory = (IHotDrinkFactory)Activator.CreateInstance(
+                    Type.GetType("Factories." + Enum.GetName(typeof(AvailableDrink), drink)+ "Factory"));
+                factories.Add(drink, factory);
+            }
+        }
+
+        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        {
+            return factories[drink].Prepare(amount);
+        }
+    }
+
     class Program
     {
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
-
+            var machine = new HotDrinkMachine();
+            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
+            drink.consume();
         }
     }
 }
