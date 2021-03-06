01962b21034a4cb40154b92680d35f194f485751
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2021-03-06 16:07:44 -0300

    Using reflections to get rid of dependencies of implementation and violating Open-Closed principles

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -71,6 +71,54 @@ namespace Factories
         //{
         //    return factories[drink].Prepare(amount);
         //}
+
+        private List<Tuple<string, IHotDrinkFactory>> factories =
+            new List<Tuple<string, IHotDrinkFactory>>();
+
+        public HotDrinkMachine()
+        {
+            foreach(var t in typeof(HotDrinkMachine).Assembly.GetTypes())
+            {
+                if(typeof(IHotDrinkFactory).IsAssignableFrom(t) &&
+                    !t.IsInterface)
+                {
+                    factories.Add(Tuple.Create(
+                        t.Name.Replace("Factory", string.Empty),
+                        (IHotDrinkFactory)Activator.CreateInstance(t)
+                        ));
+                }
+            }
+        }
+
+        public IHotDrink MakeDrink()
+        {
+            WriteLine("Available drinks:");
+            for(var index = 0; index < factories.Count; index++)
+            {
+                var tuple = factories[index];
+                WriteLine($"{index}: {tuple.Item1}");
+            }
+
+            while (true)
+            {
+                string s;
+                if ((s = Console.ReadLine()) != null
+                    && int.TryParse(s, out int i)
+                    && i >= 0
+                    && i < factories.Count)
+                {
+                    Write("Specify amount: ");
+                    s = ReadLine();
+                    if (s != null
+                        && int.TryParse(s, out int amount)
+                        && amount > 0)
+                    {
+                        return factories[i].Item2.Prepare(amount);
+                    }
+                }
+                WriteLine("Incorrect input, try again!");
+            }
+        } 
     }
 
     class Program
@@ -78,7 +126,7 @@ namespace Factories
         public static void Main(string[] args)
         {
             var machine = new HotDrinkMachine();
-            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
+            var drink = machine.MakeDrink();
             drink.consume();
         }
     }
